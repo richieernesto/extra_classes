e7ae113ae09cdeb9cb76564c0a1f2517cfcfe4c0
RichieErnest1 <richie.ernesto2@gmail.com>
2022-04-16 12:38:51 +0000

    Sign Up added

--- lib/widgets/auth_card.dart
+++ lib/widgets/auth_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum AuthMode {SignIn, SignUp}
+
 class AuthCard extends StatefulWidget {
   const AuthCard({ Key? key }) : super(key: key);
 
@@ -9,19 +11,42 @@ class AuthCard extends StatefulWidget {
 
 class _AuthCardState extends State<AuthCard> {
   final GlobalKey<FormState> _formKey = GlobalKey();
+  AuthMode authMode = AuthMode.SignIn;
+ 
+
+  void _setAuthMode(){
+    if (authMode == AuthMode.SignIn){
+      setState(() {
+        //print(authMode);
+        authMode = AuthMode.SignUp;
+        print(authMode);
+      });
+    }
+    else {
+      setState(() {
+        //print(authMode);
+        authMode = AuthMode.SignIn;
+        print(authMode);
+      });
+  }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      
       child: Card(
+        margin: EdgeInsets.all(5),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
         elevation: 10.0,
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
+            padding: EdgeInsets.all(10),
             child: Column(children: <Widget>[
               TextFormField(
                 decoration: InputDecoration(
-                  labelStyle: TextStyle(text: TextAlign.center),
+                  //labelStyle: TextStyle(text: TextAlign.center),
                   labelText: 'Username/Email'
                 ),
                 keyboardType: TextInputType.emailAddress,
@@ -32,7 +57,15 @@ class _AuthCardState extends State<AuthCard> {
                 ),
                 obscureText: true,
               ),
-              ElevatedButton(onPressed: (){}, child: Text("Sign In"))
+              authMode == AuthMode.SignUp ? TextFormField(
+                //enabled: authMode = true,
+                decoration: InputDecoration(
+                  labelText: "Confirm password",
+                ),
+                obscureText: true,
+              ) : Container(),
+              ElevatedButton(onPressed: _setAuthMode, child: Text("Sign In")),
+              ElevatedButton(onPressed:_setAuthMode, child: Text("Sign Up"))
             ]),
           ) ,
           ),
