f9507a0f23c4db5baa65f20c76a3d594960dd2a3
Richie <esselricmond78@gmail.com>
2022-04-21 18:58:19 +0000

    User Sign in Added
    Sign in added but after authentication screen redirection is not yet done.

--- lib/Models/auth.dart
+++ lib/Models/auth.dart
@@ -2,21 +2,42 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class Auth with ChangeNotifier{
+class Auth with ChangeNotifier {
   late String _token;
   late DateTime _expiryToken;
   late String user_Id;
 
   //Auth(this._token, this._expiryToken, this.user_Id);
 
-  Future <void> signUp(String email, String password)async{
-    Uri url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCxmJ-9qiP2Eh3tSFLSPJUAfRkHi1O0wKc');
-    final response =await http.post(url, body: json.encode({
-      "email": email,
-      "password": password,
-      "returnSecureToken": true
-    }) );
-    //To verify if sign up worked 
-    print (json.decode(response.body));
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    Uri url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCxmJ-9qiP2Eh3tSFLSPJUAfRkHi1O0wKc');
+    final response = await http.post(url,
+        body: json.encode(
+            {"email": email, "password": password, "returnSecureToken": true}));
+    //To verify if sign up worked
+    print(json.decode(response.body).toString());
+  }
+
+  Future<void> signUp(String email, String password) async {
+    Uri url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCxmJ-9qiP2Eh3tSFLSPJUAfRkHi1O0wKc');
+    final response = await http.post(url,
+        body: json.encode(
+            {"email": email, "password": password, "returnSecureToken": true}));
+    //To verify if sign up worked
+    print(json.decode(response.body).toString());
+  }
+
+  Future<void> login(String email, String password) async {
+    //The Function was not just called but rather the return statement was added to allow the loading spinner to work
+    Uri url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCxmJ-9qiP2Eh3tSFLSPJUAfRkHi1O0wKc');
+    final response = await http.post(url,
+        body: json.encode(
+            {"email": email, "password": password, "returnSecureToken": true}));
+    //To verify if sign up worked
+    print(json.decode(response.body).toString());
   }
 }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -5,36 +5,33 @@ import '../widgets/auth_card.dart';
 import '../widgets/loading_spinner.dart';
 import '../Models/auth.dart';
 
- enum AuthMode { SignIn, SignUp }
+enum AuthMode { SignIn, SignUp }
 
 class AuthScreen extends StatelessWidget {
-  const AuthScreen({ Key? key }) : super(key: key);
-
+  const AuthScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(
-              child: Stack(
-          children: <Widget> [
-            Container(
-              child: Image.asset('lib/assets/img/bgimage.jpg', fit: BoxFit.fitHeight,),
-             height: double.infinity,
-            //  width: double.infinity,
-              
-              ),
-              Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.only(top: 10),
-                  padding: EdgeInsets.all(35),
-                  child: AuthCard()),
-              //LoadingScreen()
-          ]
+        body: SafeArea(
+      child: Stack(children: <Widget>[
+        Container(
+          child: Image.asset(
+            'lib/assets/img/bgimage.jpg',
+            fit: BoxFit.fitHeight,
+          ),
+          height: double.infinity,
+          //  width: double.infinity,
         ),
-      )
-      
-    );
+        Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(top: 10),
+            padding: EdgeInsets.all(35),
+            child: AuthCard()),
+        //LoadingScreen()
+      ]),
+    ));
   }
 }
 
@@ -57,7 +54,7 @@ class _AuthCardState extends State<AuthCard> {
   final _emailController = TextEditingController();
   var _displaySnackBar = false;
 
-   Future <void> _submit() async {
+  Future<void> _submit() async {
     if (!_formKey.currentState!.validate()) {
       // Invalid!
       return;
@@ -68,17 +65,28 @@ class _AuthCardState extends State<AuthCard> {
     });
     if (_authMode == AuthMode.SignIn) {
       //login
+      String? mail;
+      String? pass;
+      if (_authData['email'] != null) {
+        mail = _authData['email'];
+      }
+      if (_authData['email'] != null) {
+        pass = _authData['password'];
+      }
+      await Provider.of<Auth>(context, listen: false).login(mail!, pass!);
     } else {
       String? mail;
       String? pass;
-      if(_authData['email'] != null){
-        mail= _authData['email'];
+      if (_authData['email'] != null) {
+        mail = _authData['email'];
       }
-      if (_authData['email'] !=null){
+      if (_authData['email'] != null) {
         pass = _authData['password'];
       }
-      await Provider.of<Auth>(context, listen: false).signUp(mail!, pass!, );
-      
+      await Provider.of<Auth>(context, listen: false).signUp(
+        mail!,
+        pass!,
+      );
     }
     setState(() {
       _isLoading = false;
@@ -87,11 +95,9 @@ class _AuthCardState extends State<AuthCard> {
       _authMode = AuthMode.SignIn;
       //This is to minimize the keyboard
       FocusManager.instance.primaryFocus?.unfocus();
-      if(_displaySnackBar = false){
+      if (_displaySnackBar = false) {
         _displaySnackBar = true;
       }
-      
-
     });
   }
 
@@ -129,31 +135,31 @@ class _AuthCardState extends State<AuthCard> {
                   labelText: 'Username/Email'),
               keyboardType: TextInputType.emailAddress,
               controller: _emailController,
-              validator: (value){
+              validator: (value) {
                 if (value!.isEmpty || !value!.contains('@')) {
-                      return 'Invalid email!';
-                    }
-                    //return null;
-                    //return null;
-                  },
-                onSaved: (value){
-                  _authData['email'] = value!;
-                },  
+                  return 'Invalid email!';
+                }
+                //return null;
+                //return null;
+              },
+              onSaved: (value) {
+                _authData['email'] = value!;
+              },
             ),
             TextFormField(
               decoration: InputDecoration(labelText: 'Password'),
               obscureText: true,
               controller: _passwordController,
-              validator: (value){
-                if(value!.isEmpty||value.length < 5){
+              validator: (value) {
+                if (value!.isEmpty || value.length < 5) {
                   return "Password is short";
                 }
               },
-              onSaved: (value){
+              onSaved: (value) {
                 _authData['password'] = value!;
               },
             ),
-            //This TextField should only be displayed when the user taps on sign up instead otherwise do not 
+            //This TextField should only be displayed when the user taps on sign up instead otherwise do not
             _authMode == AuthMode.SignUp
                 ? TextFormField(
                     //enabled: authMode = true,
@@ -161,31 +167,42 @@ class _AuthCardState extends State<AuthCard> {
                       labelText: "Confirm password",
                     ),
                     obscureText: true,
-                    validator: _authMode ==AuthMode.SignUp? (value){
-                      if (value!= _passwordController.text){
-                        return "Paswords do not match!";
-                      }
-                    } : null,
+                    validator: _authMode == AuthMode.SignUp
+                        ? (value) {
+                            if (value != _passwordController.text) {
+                              return "Paswords do not match!";
+                            }
+                          }
+                        : null,
                   )
                 : Container(),
-                _isLoading? Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SpinKitDualRing(color: Colors.blue, size: 30,),
-                ):
-            ElevatedButton(onPressed: _submit, child: Text(_authMode == AuthMode.SignIn?  "Sign In": "Sign Up")),
+            _isLoading
+                ? Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SpinKitDualRing(
+                      color: Colors.blue,
+                      size: 30,
+                    ),
+                  )
+                : ElevatedButton(
+                    onPressed: _submit,
+                    child: Text(
+                        _authMode == AuthMode.SignIn ? "Sign In" : "Sign Up")),
             Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 GestureDetector(
                   onTap: _setAuthMode,
-                  child: Text(_authMode==AuthMode.SignIn? "Sign up Instead": "Sign In",
+                  child: Text(
+                      _authMode == AuthMode.SignIn
+                          ? "Sign up Instead"
+                          : "Sign In",
                       style: TextStyle(
                         color: Colors.red,
                       )),
-                      
                 ),
-                
-                SizedBox(width:15),
+
+                SizedBox(width: 15),
                 //ElevatedButton(onPressed: (){}, child: Text("Submit"))
               ],
             ),
